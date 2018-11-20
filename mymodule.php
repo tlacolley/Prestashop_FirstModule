5c7804b83498d89acd390e4df4399ab1b9033490
tlacolley <thomaslacolley@gmail.com>
2018-11-20 16:51:23 +0100

    Creation and installation MyModule for Prestashop

---
+++ mymodule.php
@@ -0,0 +1,60 @@
+<?php
+if (!defined('_PS_VERSION_'))
+{
+  exit;
+}
+
+class MyModule extends Module
+{
+  public function __construct()
+  {
+    $this->name = 'mymodule';
+    $this->tab = 'front_office_features';
+    $this->version = '1.0.0';
+    $this->author = 'Firstname Lastname';
+    $this->need_instance = 0;
+    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    $this->bootstrap = true;
+
+    parent::__construct();
+
+    $this->displayName = $this->l('My module');
+    $this->description = $this->l('Description of my module.');
+
+    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+    if (!Configuration::get('MYMODULE_NAME'))
+      $this->warning = $this->l('No name provided');
+  }
+
+  public function install()
+  {
+    if (Shop::isFeatureActive())
+      Shop::setContext(Shop::CONTEXT_ALL);
+
+    if (!parent::install() ||
+      !$this->registerHook('leftColumn') ||
+      !$this->registerHook('header') ||
+      !Configuration::updateValue('MYMODULE_NAME', 'my friend')
+    )
+      return false;
+
+    return true;
+  }
+
+  public function uninstall()
+  {
+    if (!parent::uninstall() ||
+      !Configuration::deleteByName('MYMODULE_NAME')
+    )
+      return false;
+
+    return true;
+  }
+
+
+
+
+
+}
+?>
