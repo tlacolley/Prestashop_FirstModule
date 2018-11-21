7ab043e50f74dcd299c6f8065f3ba65575ff68df
tlacolley <thomaslacolley@gmail.com>
2018-11-21 11:17:55 +0100

    Add function for display a form and change a varialbe in db

--- mymodule.php
+++ mymodule.php
@@ -55,6 +55,87 @@ class MyModule extends Module
 
 
 
+public function getContent()
+{
+    $output = null;
+ 
+    if (Tools::isSubmit('submit'.$this->name))
+    {
+        $my_module_name = strval(Tools::getValue('MYMODULE_NAME'));
+        if (!$my_module_name
+          || empty($my_module_name)
+          || !Validate::isGenericName($my_module_name))
+            $output .= $this->displayError($this->l('Invalid Configuration value'));
+        else
+        {
+            Configuration::updateValue('MYMODULE_NAME', $my_module_name);
+            $output .= $this->displayConfirmation($this->l('Settings updated'));
+        }
+    }
+    return $output.$this->displayForm();
+}
+
+public function displayForm()
+{
+    // Get default language
+    $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
+ 
+    // Init Fields form array
+    $fields_form[0]['form'] = array(
+        'legend' => array(
+            'title' => $this->l('Settings'),
+        ),
+        'input' => array(
+            array(
+                'type' => 'text',
+                'label' => $this->l('Configuration value'),
+                'name' => 'MYMODULE_NAME',
+                'size' => 20,
+                'required' => true
+            )
+        ),
+        'submit' => array(
+            'title' => $this->l('Save'),
+            'class' => 'btn btn-default pull-right'
+        )
+    );
+ 
+    $helper = new HelperForm();
+ 
+    // Module, token and currentIndex
+    $helper->module = $this;
+    $helper->name_controller = $this->name;
+    $helper->token = Tools::getAdminTokenLite('AdminModules');
+    $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+ 
+    // Language
+    $helper->default_form_language = $default_lang;
+    $helper->allow_employee_form_lang = $default_lang;
+ 
+    // Title and toolbar
+    $helper->title = $this->displayName;
+    $helper->show_toolbar = true;        // false -> remove toolbar
+    $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+    $helper->submit_action = 'submit'.$this->name;
+    $helper->toolbar_btn = array(
+        'save' =>
+        array(
+            'desc' => $this->l('Save'),
+            'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+            '&token='.Tools::getAdminTokenLite('AdminModules'),
+        ),
+        'back' => array(
+            'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+            'desc' => $this->l('Back to list')
+        )
+    );
+ 
+    // Load current value
+    $helper->fields_value['MYMODULE_NAME'] = Configuration::get('MYMODULE_NAME');
+ 
+    return $helper->generateForm($fields_form);
+}
+
 
 }
 ?>
